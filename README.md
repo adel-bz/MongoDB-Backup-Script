9d9b67d12a2d2df2c25d2c72b92334ad89099fc8
a.babzadeh <adel.babzadeh@gmail.com>
2023-07-10 11:42:19 +0330

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MongoDB-Bcakup-Script
+# MongoDB-Backup-Script
+
+# Overview
+This bash script project is for the backup of your data from the MongoDB database on a Docker container and the project Assets files.
+
+# Requirements
+- You need a backup server with an FTP connection.
+
+# Environment Variables
---
+++ backup.sh
@@ -0,0 +1,50 @@
+# !/bin/bash
+
+### Reading ENV
+source $(dirname $BASH_SOURCE)/.env
+
+### Database Backup ###
+printf ""$GREEN"......... BACKUP START ......... "$NC" \t "$date" \t Done\n"
+sudo docker exec "$db_container_name" mongodump --host=localhost --port="$db_port" --username="$db_username" --password="$db_pass" --out="$db_container_volume"/"$db_prefixname"_"$date"|
+sleep 2
+mv "$db_host_volume"/"$db_prefixname"_"$date" .
+tar -czvf "$db_prefixname"_"$date".tar.gz --absolute-names "$db_prefixname"_"$date"
+
+
+### Assets Backup ###
+sleep 2
+cp -rf "$assets_host_volume" .
+tar -czvf "$assets_prefixname"_"$date".tar.gz --absolute-names "$assets_dir_name"
+echo ".........."
+
+
+### Uploading && Alert to slack
+curl -T "$db_prefixname"_"$date".tar.gz ftp://"$backup_host"/"$db_backuphost_dir"/"$db_prefixname"_"$date".tar.gz --user "$backup_host_user":"$backup_host_pass"
+if [ $? == 0 ]
+then
+  printf ""$GREEN"####### Upload Database Success"$NC""
+  echo ".........."
+curl -T "$assets_prefixname"_"$date".tar.gz ftp://"$backup_host"/"$assets_backuphost_dir"/"$assets_prefixname"_"$date".tar.gz --user "$backup_host_user":"$backup_host_pass"
+  if [ $? == 0 ]
+  then
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"*#### Backup - database & assets Backup success "}' "$slack_url"
+    printf ""$GREEN"####### Upload Assets Success "$NC""
+    echo ".........."
+  else
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"*#### Backup - database Backup success but assets Backup faild "}' "$slack_url"
+    printf ""$RED"####### Database Backup And Upload Success But Assets Upload Or Backup Faild"
+    echo ".........."
+  fi
+else
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"*#### Backup - database & assets Backup faild "}' "$slack_url"
+  printf ""$RED"####### Database & Assets Backup Or Upload Failds"
+  echo ".........."
+fi
+
+### Cleaning
+rm -r "$db_prefixname"* "$assets_dir_name"
+rm *.gz
+echo ".........."
+printf ""$GREEN"####### Cleaned "$NC" \t "$date" \t Done\n"
+
+printf ""$GREEN"......... BACKUP FINISHED ......... "$NC" \t "$date" \t Done\n"
